f397315245959604aa8563b5c450175cf6133d92
Fabian Braun <fsbraun@gmx.de>
2022-03-31 08:48:51 +0200

    isort... 🤪

--- djangocms_text_ckeditor/cms_toolbars.py
+++ djangocms_text_ckeditor/cms_toolbars.py
@@ -1,19 +1,23 @@
-from cms.cms_toolbars import CMSToolbar
-from cms.toolbar.items import BaseItem
-from cms.toolbar_pool import toolbar_pool
 from django import forms
 from django.templatetags.static import static
 from django.utils.safestring import mark_safe
+
+from cms.cms_toolbars import CMSToolbar
+from cms.toolbar.items import BaseItem
+from cms.toolbar_pool import toolbar_pool
+
 from djangocms_text_ckeditor import settings
 
 
 class InlineEditingItem(BaseItem):
     """Make ckeditor config available for inline editing"""
+
     def render(self):
         return mark_safe(
             f'<script class="ckeditor5-config" '
             f'data-static_url="{ static("djangocms_text_ckeditor") }" '
-            f'data-ckeditor_basepath="{settings.TEXT_CKEDITOR_BASE_PATH}"></script>')
+            f'data-ckeditor_basepath="{settings.TEXT_CKEDITOR_BASE_PATH}"></script>'
+        )
 
 
 @toolbar_pool.register
@@ -23,11 +27,12 @@ class InlineEditingToolbar(CMSToolbar):
         if self.toolbar.edit_mode_active:
             return forms.Media(
                 css={"all": ("djangocms_text_ckeditor/css/cms.ckeditor.inline.css",)},
-                js=("djangocms_text_ckeditor/js/dist/bundle.ckeditor5.js",
-                    "djangocms_text_ckeditor/js/cms.ckeditor5.js")
+                js=(
+                    "djangocms_text_ckeditor/js/dist/bundle.ckeditor5.js",
+                    "djangocms_text_ckeditor/js/cms.ckeditor5.js",
+                ),
             )
         return forms.Media()
 
     def populate(self):
         self.toolbar.add_item(InlineEditingItem(), position=None)
-
